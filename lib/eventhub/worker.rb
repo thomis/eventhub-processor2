d96a7e184b7d4d24283e151d143fd55dc264a9df
Steiner, Thomas <thomas.steiner@novartis.com>
2017-11-30 01:21:41 +0100

    ! code style

--- lib/eventhub/worker.rb
+++ lib/eventhub/worker.rb
@@ -23,12 +23,19 @@ module Eventhub
       wait_for_stop
     end
 
+    def self.restart
+      stop
+      start
+    end
+
+    private
+
     def self.wait_for_stop
       workers = WORKER.keys
       start = Time.now
 
-      # as long as we have running workers and time below max stop time
-      while !workers.empty?  && (Time.now - start < MAX_STOP_TIME_IN_S)
+      # as long as we have running workers and below time limit
+      while !workers.empty? && below_time_limit?(start)
         if @@queue_terminated.size > 0
           workers.delete(@@queue_terminated.pop)
         else
@@ -40,9 +47,9 @@ module Eventhub
       Eventhub.logger.warn(msg) unless workers.empty?
     end
 
-    def self.restart
-      stop
-      start
+    def self.below_time_limit?(start)
+      (Time.now - start) < MAX_STOP_TIME_IN_S
     end
+
   end
 end
