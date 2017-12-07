30fec110747eae03d92b3181f38142e194aa382b
Steiner, Thomas <thomas.steiner@ikey.ch>
2017-12-07 08:06:43 +0100

    ! code style

--- lib/eventhub/hash_extensions.rb
+++ lib/eventhub/hash_extensions.rb
@@ -6,7 +6,8 @@ module HashExtensions
 
   # InstanceMethods module
   module InstanceMethods
-    # get value from provided key path, e.g. hash.get(%w(event_hub plate.queue1 retry_s))
+    # get value from provided key path
+    # e.g. hash.get(%w(event_hub plate.queue1 retry_s))
     # "a" => { "b" => { "c" => { "value"}}}
     def get(arg)
       path = arg.is_a?(String) ? arg.split('.') : arg
@@ -16,7 +17,7 @@ module HashExtensions
     # set value from provided key path, e.h. hash.set('a.b.c','new value')
     # if overwrite is false, value will be set if it was nil previously
     def set(arg, value, overwrite = true)
-      *key_path, last = arg.is_a?(String) ? arg.split(".") : arg
+      *key_path, last = arg.is_a?(String) ? arg.split('.') : arg
       if overwrite
         key_path.inject(self) { |h, key| h.key?(key) ? h[key] :  h[key] = {} } [last] = value
       else
@@ -24,12 +25,13 @@ module HashExtensions
       end
     end
 
-    # get all keys path, { 'a' => 'value1', 'b' => { 'c' => 'value2'}}.all_keys_with_path => ['a','b.c']
+    # get all keys with path,
+    # { 'a' => 'v1', 'b' => { 'c' => 'v2'}}.all_keys_with_path => ['a','b.c']
     def all_keys_with_path(parent = nil)
       a = []
       each do |k, v|
         if v.is_a?(Hash)
-          a << v.all_keys_with_path([parent,k].compact.join('.'))
+          a << v.all_keys_with_path([parent, k].compact.join('.'))
         else
           a << [parent, k].compact.join('.').to_s
         end
