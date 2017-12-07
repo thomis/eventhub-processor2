a2dd6cc672bd64532e5240f2d5d9708893b536fc
Steiner, Thomas <thomas.steiner@ikey.ch>
2017-12-07 07:51:25 +0100

    ! code style

--- lib/eventhub/message.rb
+++ lib/eventhub/message.rb
@@ -4,7 +4,7 @@ module EventHub
   class Message
     include Helper
 
-    VERSION = '1.0.0'
+    VERSION = '1.0.0'.freeze
 
     # Headers that are required (value can be nil) in order to pass valid?
     REQUIRED_HEADERS = [
@@ -20,26 +20,26 @@ module EventHub
       'status.retried_count',
       'status.code',
       'status.message'
-    ]
+    ].freeze
 
     attr_accessor :header, :body, :raw, :vhost, :routing_key
 
     # Build accessors for all required headers
     REQUIRED_HEADERS.each do |header|
-      name = header.tr('.','_')
+      name = header.tr('.', '_')
 
       define_method(name) do
         self.header.get(header)
       end
 
       define_method("#{name}=") do |value|
-        self.header.set(header,value)
+        self.header.set(header, value)
       end
     end
 
     def self.from_json(raw)
       data = JSON.parse(raw)
-      Message.new(data.get('header'), data.get('body'),raw)
+      Message.new(data.get('header'), data.get('body'), raw)
     rescue => e
       Message.new(
         {
@@ -83,7 +83,7 @@ module EventHub
       # check for existence and defined value
       REQUIRED_HEADERS.all? do |key|
         @header.all_keys_with_path.include?(key) &&
-        !self.send(key.tr('.','_').to_sym).nil?
+          !send(key.tr('.', '_').to_sym).nil?
       end
     end
 
@@ -120,11 +120,11 @@ module EventHub
     end
 
     def to_json
-      {'header' => self.header, 'body' => self.body}.to_json
+      { 'header' => header, 'body' => body }.to_json
     end
 
     def to_s
-      "Msg: process "\
+      'Msg: process '\
         "[#{process_name}, #{process_step_position}, #{process_execution_id}]"\
         ", status [#{status_code},#{status_message},#{status_retried_count}]"
     end
@@ -132,8 +132,8 @@ module EventHub
     # copies the message and set's provided status code (default: success), actual stamp, and a new message id
     def copy(status_code = STATUS_SUCCESS)
       # use Marshal dump and load to make a deep object copy
-      copied_header = Marshal.load( Marshal.dump(header))
-      copied_body   = Marshal.load( Marshal.dump(body))
+      copied_header = Marshal.load(Marshal.dump(header))
+      copied_body   = Marshal.load(Marshal.dump(body))
 
       copied_header.set('message_id', UUIDTools::UUID.timestamp_create.to_s)
       copied_header.set('created_at', now_stamp)
@@ -146,7 +146,7 @@ module EventHub
       header.set('execution_history', []) unless \
         header.get('execution_history')
       header.get('execution_history') << \
-        {'processor' => processor_name, 'timestamp' => now_stamp}
+        { 'processor' => processor_name, 'timestamp' => now_stamp }
     end
 
     def self.translate_status_code(code)
