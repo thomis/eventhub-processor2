77e661cb1b261bddf9033c0f3d38cfc785f36ac6
Steiner, Thomas <thomas.steiner@ikey.ch>
2017-12-07 08:03:34 +0100

    ! code style

--- lib/eventhub/hash_extensions.rb
+++ lib/eventhub/hash_extensions.rb
@@ -1,40 +1,41 @@
+# HashExtensions module
 module HashExtensions
-
+  # ClassMethods module
   module ClassMethods
   end
 
+  # InstanceMethods module
   module InstanceMethods
     # get value from provided key path, e.g. hash.get(%w(event_hub plate.queue1 retry_s))
     # "a" => { "b" => { "c" => { "value"}}}
     def get(arg)
       path = arg.is_a?(String) ? arg.split('.') : arg
-      path.inject(self,:[])
+      path.inject(self, :[])
     end
 
     # set value from provided key path, e.h. hash.set('a.b.c','new value')
     # if overwrite is false, value will be set if it was nil previously
-    def set(arg,value,overwrite=true)
+    def set(arg, value, overwrite = true)
       *key_path, last = arg.is_a?(String) ? arg.split(".") : arg
       if overwrite
-        key_path.inject(self) { |h,key| h.has_key?(key) ? h[key] :  h[key]={}} [last] = value
+        key_path.inject(self) { |h, key| h.key?(key) ? h[key] :  h[key] = {} } [last] = value
       else
-        key_path.inject(self) { |h,key| h.has_key?(key) ? h[key] :  h[key]={}} [last] ||= value
+        key_path.inject(self) { |h, key| h.key?(key) ? h[key] :  h[key] = {} } [last] ||= value
       end
     end
 
     # get all keys path, { 'a' => 'value1', 'b' => { 'c' => 'value2'}}.all_keys_with_path => ['a','b.c']
-    def all_keys_with_path(parent=nil)
+    def all_keys_with_path(parent = nil)
       a = []
-      each do |k,v|
+      each do |k, v|
         if v.is_a?(Hash)
           a << v.all_keys_with_path([parent,k].compact.join('.'))
         else
-          a << "#{[parent,k].compact.join(".")}"
+          a << [parent, k].compact.join('.').to_s
         end
       end
       a.flatten
     end
-
   end
 
   def self.included(receiver)
