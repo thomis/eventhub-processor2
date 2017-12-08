25891ebe1dedfbac7184f6335c990580866cb947
Steiner, Thomas <thomas.steiner@ikey.ch>
2017-12-08 10:45:58 +0100

    ! rspec

--- spec/eventhub/listener_spec.rb
+++ spec/eventhub/listener_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe EventHub::ActorListener do
-  before(:all) do
+  before(:each) do
     @listener = EventHub::ActorListener.new(EventHub::Processor2.new)
-    sleep 0.5
   end
 
   it 'gives a valid actor' do
