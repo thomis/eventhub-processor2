3409ca84afa753d7da818f02862f6a653444fea5
thomis <thomas.steiner@novartis.com>
2018-01-10 20:33:25 +0100

    + more specs

--- spec/eventhub/processor2_spec.rb
+++ spec/eventhub/processor2_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'fileutils'
 
 RSpec.describe EventHub::Processor2 do
   around(:each) do |example|
@@ -29,4 +30,32 @@ RSpec.describe EventHub::Processor2 do
     thr.join
     expect(true).to eq(true)
   end
+
+  it 'reloads configuration file' do
+    config_file_content = '{ "development": { "processor": { "heartbeat_cycle_in_s": 60 }}}'
+
+    processor = EventHub::Processor2.new
+    thr = Thread.new { processor.start }
+    sleep 0.5
+
+    # write new configuration file
+    IO.write('./config/processor2.json', config_file_content)
+
+    # send signal to reload configuration file
+    Process.kill 'HUP', 0
+    sleep 1
+
+    # check value again
+    expect(EventHub::Configuration.processor[:heartbeat_cycle_in_s]).to eq(60)
+
+    sleep 1
+    processor.stop
+    thr.join
+    expect(true).to eq(true)
+
+    # remove config folder with files
+    FileUtils.rm('config/processor2.json')
+  end
+
+
 end
